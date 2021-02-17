3a57308651fc57ea948e5a7a513612cc4cbf27be
YD <yadunayeu@gmail.com>
2021-02-17 15:22:58 +0100

    step 9, autoShowing pictures

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import cheeckTextInputs from './modules/cheeckTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
+import picture from './modules/picture';
 
 window.document.addEventListener('DOMContentLoaded', () => {
     "use strict";
@@ -20,4 +21,5 @@ window.document.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('.button-styles', '#styles .row');
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
+    picture('.sizes-block');
 });
